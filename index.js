2662d810a3f282fe7b295cb6b0217e21b0a21999
wminaung <minwin243@gmail.com>
2023-02-08 18:47:43 +0630

    try to log

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 const express = require("express");
 const { homeRouter } = require("./routes/homeRouter");
-
+const { writeFileSync } = require("fs");
 const app = express();
+app.use(express.json());
 
-app.set("views", "./views");
-app.set("view engine", "pug");
+const users = [
+  { name: "win", password: "win123" },
+  { name: "min", password: "min123" },
+];
 
-app.get("/profile", (req, res) => {
-  res.render("profile", { title: "Hey", message: "Hello there!" });
+// const auth = (req, res, next) => {
+//   const { iname, ipass } = req.body;
+//   let isOk = false;
+//   users.forEach((user) => {
+//     if (user.name === iname && user.password === ipass) {
+//       isOk = true;
+//     }
+//   });
+
+//   if (isOk) {
+//     next();
+//   } else {
+//     res.send("Nope You can't login");
+//   }
+// };
+// app.post("/", (req, res) => {
+//   res.send("Hello login");
+// });
+
+app.use((req, res, next) => {
+  const date = new Date().toUTCString();
+  const reqUrl = req.url;
+  const method = req.method;
+  const log = `${method} - ${reqUrl} | ${date} \n`;
+  writeFileSync("./request.log", log, { flag: "a" });
+  next();
 });
 
 app.use("/home", homeRouter);
 
 app.listen(3000, () => {
-  console.log("Server is listen in PORT : 3000");
+  console.log("Server is listen on PORT : 3000");
 });
